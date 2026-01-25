5480d189a2627e3543f74b0501e33717a21d1d6b
mpmaster128 <armykingsuleka@gmail.com>
2026-01-25 18:18:02 +0200

    Update SESSION_ID and OWNER_NUMBER in settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", // Put Session Id Here Inside "Ice~FpzDyi"
+  OWNER_NUMBER: "260571100479", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY DARRELL MUCHERI ( MR ULEKA KING )
